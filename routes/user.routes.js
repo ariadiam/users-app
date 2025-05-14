8eac80c25bbc72cb27b2ad5f6d0717c04e628978
Ar. Diamanti <ariadiamanti@gmail.com>
2025-05-14 11:55:02 +0300

    fixes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,10 +5,11 @@ const userController = require('../controllers/user.controller');
 const verifyToken = require('../middlewares/auth.middleware').verifyToken
 const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
-router.get('/', userController.findAll);
-router.get('/:username', userController.findOne);
+router.get('/',verifyToken, userController.findAll);
+router.get('/:username', verifyToken, userController.findOne);
 // router.post('/',userController.create);
-router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
+// router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
+router.post('/', userController.create);
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update);
 router.delete('/:username', verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername);
 router.delete('/:username/email/:email', verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail )
